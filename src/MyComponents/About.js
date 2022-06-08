de3f0ddc07169d52113272646f997cfe55493e06
Jashan Warraich <Wjashan2220@gmail.com>
2022-06-08 21:01:16 +0530

    Add files via upload

---
+++ src/MyComponents/About.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div>This is an About component
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas ipsa dolore fuga fugiat nostrum aut expedita velit. Rerum, deserunt a libero unde dolorem reprehenderit doloribus expedita, consectetur eos illum, iure architecto quam voluptates dolorum.</p>
+    </div>
+  )
+}
---
+++ src/MyComponents/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const Footer = () => {
+  // let footerstyle={
+  //   position:"relative",
+  //   top:"10vh",
+  //   width:"100%",
+  //   backgroundColor:"red"
+  // }
+  return (
+    <footer className="bg-dark text-light py-3" >
+        <p className="text-center">
+          Copyright &copy; MyTodosList.com
+        </p> 
+      </footer>
+  )
+}
